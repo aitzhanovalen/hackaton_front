8d56db7c2c8620078c9815ff85aff92027c69526
Alen <aitzhanovalen@gmail.com>
2021-11-27 12:44:18 +0600

    third

--- lib/models/auth_manager.dart
+++ lib/models/auth_manager.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AuthManager extends ChangeNotifier {
-  String customerId;
-  String merchantController.text = '0000002';
+  String? customerId;
+  String? merchantId;
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,5 +1,5 @@
 export 'first_screen.dart';
-export 'merchant_from.dart';
+export 'merchant_form.dart';
 export 'product_detail.dart';
 export 'products.dart';
 export 'sales_chart.dart';
