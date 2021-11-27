bef98531f334fc65ba3afd56ffa647a9064ad30a
Alen <aitzhanovalen@gmail.com>
2021-11-27 16:44:40 +0600

    6

--- lib/screens/sales_chart.dart
+++ lib/screens/sales_chart.dart
@@ -5,9 +5,10 @@ import 'package:hack/models/auth_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:http/http.dart' as http;
+import 'package:editable/editable.dart';
 
 class SalesChart extends StatefulWidget {
-  SalesChart({Key? key}) : super(key: key);
+  const SalesChart({Key? key}) : super(key: key);
 
   @override
   _SalesChartState createState() => _SalesChartState();
@@ -23,7 +24,9 @@ class _PieData {
   final num yData;
 }
 
-class _SalesChartState extends State<SalesChart> {
+class _SalesChartState extends State<SalesChart>
+    with SingleTickerProviderStateMixin {
+  int _tabIndex = 0;
   List<_PieData> pieData = [
     // _PieData('Jan', 35, ),
     // _PieData('Feb', 28, ),
@@ -32,13 +35,22 @@ class _SalesChartState extends State<SalesChart> {
     // _PieData('May', 40, )
   ];
   String merchantId = '1000001';
+  late TabController _tabController;
 
   @override
   void initState() {
-    merchantId = Provider.of<AuthManager>(context, listen: false).merchantId ?? '';
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      print(_tabController.index);
+      setState(() {
+        _tabIndex = _tabController.index;
+      });
+    });
+    merchantId =
+        Provider.of<AuthManager>(context, listen: false).merchantId ?? '';
     http
         .get(Uri.parse(
-        'https://safe-beach-59767.herokuapp.com/merchant/ctlg/?id=$merchantId'))
+            'https://safe-beach-59767.herokuapp.com/merchant/ctlg/?id=$merchantId'))
         .then((value) {
       var response = jsonDecode(value.body);
       print(response);
@@ -57,71 +69,131 @@ class _SalesChartState extends State<SalesChart> {
     });
     super.initState();
   }
+
   @override
   void didChangeDependencies() {
-
     super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
+    bool _value = false;
+    int val = -1;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Syncfusion Flutter chart'),
+        bottom: TabBar(
+          isScrollable: true,
+          controller: _tabController,
+          unselectedLabelColor: Colors.black,
+          tabs: [
+            const Tab(
+              child: Text('Cashback'),
+            ),
+            const Tab(
+              child: Text('Продажи'),
+            ),
+            const Tab(
+              child: Text('Настройки'),
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
-          //Initialize the chart widget
-          SfCircularChart(
-              title: ChartTitle(text: 'Sales by sales person'),
-              legend: Legend(isVisible: true),
-              series: <PieSeries<_PieData, String>>[
-                PieSeries<_PieData, String>(
-                    explode: true,
-                    explodeIndex: 0,
-                    dataSource: pieData,
-                    xValueMapper: (_PieData data, _) => data.xData,
-                    yValueMapper: (_PieData data, _) => data.yData,
-                    // dataLabelMapper: (_PieData data, _) => data.text,
-                    dataLabelSettings: DataLabelSettings(isVisible: true)),
-              ]),
-          // SfCartesianChart(
-          //   primaryXAxis: CategoryAxis(),
-          //   // Chart title
-          //   title: ChartTitle(text: 'Half yearly sales analysis'),
-          //   // Enable legend
-          //   legend: Legend(isVisible: true),
-          //   // Enable tooltip
-          //   tooltipBehavior: TooltipBehavior(enable: true),
-          //   series: <ChartSeries<_SalesData, String>>[
-          //     LineSeries<_SalesData, String>(
-          //         dataSource: data,
-          //         xValueMapper: (_SalesData sales, _) => sales.year,
-          //         yValueMapper: (_SalesData sales, _) => sales.sales,
-          //         name: 'Sales',
-          //         // Enable data label
-          //         dataLabelSettings: DataLabelSettings(isVisible: true))
-          //   ],
-          // ),
-          // Expanded(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(8.0),
-          //     //Initialize the spark charts widget
-          //     child: SfSparkLineChart.custom(
-          //       //Enable the trackball
-          //       trackball: SparkChartTrackball(
-          //           activationMode: SparkChartActivationMode.tap),
-          //       //Enable marker
-          //       marker: SparkChartMarker(
-          //           displayMode: SparkChartMarkerDisplayMode.all),
-          //       //Enable data label
-          //       labelDisplayMode: SparkChartLabelDisplayMode.all,
-          //       xValueMapper: (int index) => data[index].year,
-          //       yValueMapper: (int index) => data[index].sales,
-          //       dataCount: 5,
-          //     ),
-          //   ),
-          // )
+          if (_tabIndex == 0)
+            SfCircularChart(
+                title: ChartTitle(text: 'Sales by sales person'),
+                legend: Legend(isVisible: true),
+                series: <PieSeries<_PieData, String>>[
+                  PieSeries<_PieData, String>(
+                      explode: true,
+                      explodeIndex: 0,
+                      dataSource: pieData,
+                      xValueMapper: (_PieData data, _) => data.xData,
+                      yValueMapper: (_PieData data, _) => data.yData,
+                      // dataLabelMapper: (_PieData data, _) => data.text,
+                      dataLabelSettings:
+                          const DataLabelSettings(isVisible: true)),
+                ]),
+          if (_tabIndex == 2)
+            TextButton(
+              onPressed: () {
+                //TODO save route
+                print(Provider.of<AuthManager>(context, listen: false)
+                    .merchantId);
+              },
+              child: const Text("Сохранить"),
+            ),
+          if (_tabIndex == 2)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Row(children: [
+                //   Gesture
+                // ],)
+                ListTile(
+                  title: Text("По количеству продаж"),
+                  leading: Radio(
+                    value: 1,
+                    groupValue: val,
+                    onChanged: (value) {
+                      setState(() {
+                        val = value == true ? 1 : 2;
+                      });
+                    },
+                    activeColor: Colors.green,
+                  ),
+                ),
+                ListTile(
+                  title: Text("По общей сумме"),
+                  leading: Radio(
+                    value: 2,
+                    groupValue: val,
+                    onChanged: (value) {
+                      setState(() {
+                        val = value == true ? 2 : 1;
+                      });
+                    },
+                    activeColor: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          if (_tabIndex == 2)
+            Expanded(
+              child: Editable(
+                columns: cols,
+                rows: rows,
+                zebraStripe: true,
+                stripeColor1: Colors.blue.shade100,
+                stripeColor2: Colors.grey.shade200,
+                onRowSaved: (value) {
+                  print(value);
+                },
+                onSubmitted: (value) {
+                  print(value);
+                },
+                borderColor: Colors.blueGrey,
+                tdStyle: const TextStyle(fontWeight: FontWeight.bold),
+                trHeight: 80,
+                thStyle:
+                    const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                thAlignment: TextAlign.center,
+                thVertAlignment: CrossAxisAlignment.end,
+                thPaddingBottom: 3,
+                saveIconColor: Colors.black,
+                tdAlignment: TextAlign.left,
+                tdEditableMaxLines: 100, // don't limit and allow data to wrap
+                tdPaddingTop: 0,
+                tdPaddingBottom: 14,
+                tdPaddingLeft: 10,
+                tdPaddingRight: 8,
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                    borderRadius: BorderRadius.all(Radius.circular(0))),
+              ),
+            ),
         ],
       ),
     );
@@ -165,22 +237,22 @@ List<dynamic> getCustomers(Map<String, dynamic> response) {
 
 Map<String, dynamic> settings = {
   "status": 200,
-  "merchant_id":"100001",
-  "cashback_type":"sum",// "count"
-  "data":[
-    {"sum_min":0,
-      "sum_max":50000,
-      "cashback":1.0
+  "merchant_id": "100001",
+  "cashback_type": "sum", // "count"
+  "cashback": {
+    "high": {
+      "cashback": 5.0,
+      "range": [21]
     },
-    {"sum_min":50001,
-      "sum_max":100000,
-      "cashback":1.5
+    "medium": {
+      "cashback": 3.5,
+      "range": [11, 20]
     },
-    {"sum_min":100001,
-      "sum_max":500000000000,
-      "cashback":2.0
-    }
-  ]
+    "low": {
+      "cashback": 1.5,
+      "range": [1, 10]
+    },
+  },
 };
 
 Map<String, dynamic> response = {
@@ -192,11 +264,7 @@ Map<String, dynamic> response = {
       "client_id": "1000003",
       "price": 10000,
       "cashback": 200,
-      "cashback_percent": {
-        "bank": 1,
-        "payment_system": 0.5,
-        "merchant": 0.5
-      }
+      "cashback_percent": {"bank": 1, "payment_system": 0.5, "merchant": 0.5}
     },
     {
       "sku": "111445GA8",
@@ -232,3 +300,26 @@ Map<String, dynamic> response = {
     }
   ]
 };
+
+List rows = [
+  {"name": 'low', "range_from": 1, "range_to": 10, "cashback": 2.0},
+  {
+    "name": 'medium',
+    "range_from": 11,
+    "range_to": 20,
+    "cashback": 3.5,
+  },
+  {"name": 'high', "range_from": 21, "range_to": 500, "cashback": 5.0},
+];
+
+List cols = [
+  {
+    "title": 'Наименование',
+    'widthFactor': 0.2,
+    'key': 'name',
+    'editable': false
+  },
+  {"title": 'От', 'widthFactor': 0.2, 'key': 'range_from'},
+  {"title": 'До', 'widthFactor': 0.2, 'key': 'range_to'},
+  {"title": 'cashback', 'key': 'cashback'},
+];
