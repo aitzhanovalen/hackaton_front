8b2b673672169cc1227037a6985645b44de58348
Alen <aitzhanovalen@gmail.com>
2021-11-28 09:39:28 +0600

    14

--- lib/screens/sales_chart.dart
+++ lib/screens/sales_chart.dart
@@ -30,6 +30,7 @@ class _SalesChartState extends State<SalesChart>
   String val = '';
   List<_PieData> pieData = [];
   String merchantId = '1000001';
+  List orders = [];
   late TabController _tabController;
   @override
   void initState() {
@@ -47,6 +48,7 @@ class _SalesChartState extends State<SalesChart>
         .then((value) {
       var responseOrders = jsonDecode(value.body);
       setState(() {
+        orders = responseOrders['data'];
         List values = cashback(responseOrders);
         pieData = [
           _PieData('Bank', values[0]),
@@ -104,148 +106,158 @@ class _SalesChartState extends State<SalesChart>
           ],
         ),
       ),
-      body: Column(
-        children: [
-          if (_tabIndex == 0)
-            SfCircularChart(
-              title: ChartTitle(text: 'Sales by sales person'),
-              legend: Legend(isVisible: true),
-              series: <PieSeries<_PieData, String>>[
-                PieSeries<_PieData, String>(
-                    explode: true,
-                    explodeIndex: 0,
-                    dataSource: pieData,
-                    xValueMapper: (_PieData data, _) => data.xData,
-                    yValueMapper: (_PieData data, _) => data.yData,
-                    dataLabelSettings:
-                        const DataLabelSettings(isVisible: true)),
-              ],
-            ),
-          if (_tabIndex == 2)
-            TextButton(
-              onPressed: () async {
-                //TODO save route
-                String merchantId =
-                    Provider.of<AuthManager>(context, listen: false)
-                            .merchantId ??
-                        '';
-                saveSettings(
-                  rows: rows,
-                  merchantId: merchantId,
-                  cashbackType: val,
-                );
-                final kek = await http.post(
-                  Uri.parse(
-                      'https://safe-beach-59767.herokuapp.com/merchant/create_cashback_percents'),
-                  headers: <String, String>{
-                    'Content-Type': 'application/json; charset=UTF-8',
-                  },
-                  body: jsonEncode(<String, dynamic>{
-                    "merchant_id": merchantId,
-                    "cashback_type": "sum",
-                    "cashback": {
-                      "high": {
-                        "cashback": rows[0]['cashback'],
-                        "range": [
-                          rows[0]['range_from'],
-                          rows[0]['range_to'],
-                        ]
-                      },
-                      "medium": {
-                        "cashback": rows[1]['cashback'],
-                        "range": [
-                          rows[1]['range_from'],
-                          rows[1]['range_to'],
-                        ]
-                      },
-                      "low": {
-                        "cashback": rows[2]['cashback'],
-                        "range": [
-                          rows[2]['range_from'],
-                          rows[2]['range_to'],
-                        ]
-                      }
-                    }
-                  }),
-                );
-                print(kek.statusCode);
-                print(kek.body);
-                print(makeAnswerMap(rows, merchantId, 'sum'));
-
-                print(merchantId);
-              },
-              child: const Text("Сохранить"),
-            ),
-          if (_tabIndex == 2)
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                ListTile(
-                  title: const Text("По общей сумме"),
-                  leading: Radio(
-                    value: 'sum',
-                    groupValue: val,
-                    onChanged: (value) {
-                      setState(() {
-                        val = 'sum';
-                      });
-                    },
-                    activeColor: Colors.green,
-                  ),
-                ),
-                ListTile(
-                  title: const Text("По количеству продаж"),
-                  leading: Radio(
-                    value: 'count',
-                    groupValue: val,
-                    onChanged: (value) {
-                      setState(() {
-                        val = 'count';
-                      });
-                    },
-                    activeColor: Colors.green,
-                  ),
-                ),
-              ],
-            ),
-          if (_tabIndex == 2)
-            Expanded(
-              child: Editable(
-                columns: cols,
-                rows: rows,
-                zebraStripe: true,
-                stripeColor1: Colors.blue.shade100,
-                stripeColor2: Colors.grey.shade200,
-                onRowSaved: (value) {
-                  saveTable(value);
-                },
-                onSubmitted: (value) {
-                  // print(rows);
-                },
-                borderColor: Colors.blueGrey,
-                tdStyle: const TextStyle(fontWeight: FontWeight.bold),
-                trHeight: 80,
-                thStyle:
-                    const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                thAlignment: TextAlign.center,
-                thVertAlignment: CrossAxisAlignment.center,
-                thPaddingBottom: 3,
-                saveIcon: Icons.save,
-                showSaveIcon: true,
-                saveIconColor: Colors.black,
-                tdAlignment: TextAlign.center,
-                tdEditableMaxLines: 100, // don't limit and allow data to wrap
-                tdPaddingTop: 0,
-                tdPaddingBottom: 14,
-                tdPaddingLeft: 10,
-                tdPaddingRight: 8,
-                focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.all(Radius.circular(0))),
+      body: _tabIndex == 1
+          ? ListView.builder(
+              itemCount: orders.length,
+              itemBuilder: (context, index) => NotificationCard(
+                title: orders[index]['title'].toString(),
+                customer: orders[index]['customer_id'].toString(),
+                price: orders[index]['price'].toString(),
+                cashback: orders[index]['cashback_percent'].toString(),
               ),
+            )
+          : Column(
+              children: [
+                if (_tabIndex == 0)
+                  SfCircularChart(
+                    title: ChartTitle(text: 'Sales by sales person'),
+                    legend: Legend(isVisible: true),
+                    series: <PieSeries<_PieData, String>>[
+                      PieSeries<_PieData, String>(
+                          explode: true,
+                          explodeIndex: 0,
+                          dataSource: pieData,
+                          xValueMapper: (_PieData data, _) => data.xData,
+                          yValueMapper: (_PieData data, _) => data.yData,
+                          dataLabelSettings:
+                              const DataLabelSettings(isVisible: true)),
+                    ],
+                  ),
+                if (_tabIndex == 2)
+                  TextButton(
+                    onPressed: () async {
+                      String merchantId =
+                          Provider.of<AuthManager>(context, listen: false)
+                                  .merchantId ??
+                              '';
+                      saveSettings(
+                        rows: rows,
+                        merchantId: merchantId,
+                        cashbackType: val,
+                      );
+                      final kek = await http.post(
+                        Uri.parse(
+                            'https://safe-beach-59767.herokuapp.com/merchant/create_cashback_percents'),
+                        headers: <String, String>{
+                          'Content-Type': 'application/json; charset=UTF-8',
+                        },
+                        body: jsonEncode(<String, dynamic>{
+                          "merchant_id": merchantId,
+                          "cashback_type": "sum",
+                          "cashback": {
+                            "high": {
+                              "cashback": rows[0]['cashback'],
+                              "range": [
+                                rows[0]['range_from'],
+                                rows[0]['range_to'],
+                              ]
+                            },
+                            "medium": {
+                              "cashback": rows[1]['cashback'],
+                              "range": [
+                                rows[1]['range_from'],
+                                rows[1]['range_to'],
+                              ]
+                            },
+                            "low": {
+                              "cashback": rows[2]['cashback'],
+                              "range": [
+                                rows[2]['range_from'],
+                                rows[2]['range_to'],
+                              ]
+                            }
+                          }
+                        }),
+                      );
+                      print(kek.statusCode);
+                      print(kek.body);
+                      print(makeAnswerMap(rows, merchantId, 'sum'));
+
+                      print(merchantId);
+                    },
+                    child: const Text("Сохранить"),
+                  ),
+                if (_tabIndex == 2)
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        title: const Text("По общей сумме"),
+                        leading: Radio(
+                          value: 'sum',
+                          groupValue: val,
+                          onChanged: (value) {
+                            setState(() {
+                              val = 'sum';
+                            });
+                          },
+                          activeColor: Colors.green,
+                        ),
+                      ),
+                      ListTile(
+                        title: const Text("По количеству продаж"),
+                        leading: Radio(
+                          value: 'count',
+                          groupValue: val,
+                          onChanged: (value) {
+                            setState(() {
+                              val = 'count';
+                            });
+                          },
+                          activeColor: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                if (_tabIndex == 2)
+                  Expanded(
+                    child: Editable(
+                      columns: cols,
+                      rows: rows,
+                      zebraStripe: true,
+                      stripeColor1: Colors.blue.shade100,
+                      stripeColor2: Colors.grey.shade200,
+                      onRowSaved: (value) {
+                        saveTable(value);
+                      },
+                      onSubmitted: (value) {
+                        // print(rows);
+                      },
+                      borderColor: Colors.blueGrey,
+                      tdStyle: const TextStyle(fontWeight: FontWeight.bold),
+                      trHeight: 80,
+                      thStyle: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                      thAlignment: TextAlign.center,
+                      thVertAlignment: CrossAxisAlignment.center,
+                      thPaddingBottom: 3,
+                      saveIcon: Icons.save,
+                      showSaveIcon: true,
+                      saveIconColor: Colors.black,
+                      tdAlignment: TextAlign.center,
+                      tdEditableMaxLines:
+                          100, // don't limit and allow data to wrap
+                      tdPaddingTop: 0,
+                      tdPaddingBottom: 14,
+                      tdPaddingLeft: 10,
+                      tdPaddingRight: 8,
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                          borderRadius: BorderRadius.all(Radius.circular(0))),
+                    ),
+                  ),
+              ],
             ),
-        ],
-      ),
     );
   }
 }
@@ -344,7 +356,8 @@ Future<void> saveSettings({
   required String cashbackType,
 }) async {
   final kek = await http.post(
-    Uri.parse('https://safe-beach-59767.herokuapp.com/merchant/create_cashback_percents'),
+    Uri.parse(
+        'https://safe-beach-59767.herokuapp.com/merchant/create_cashback_percents'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
@@ -356,4 +369,70 @@ Future<void> saveSettings({
   );
   print(kek.body);
   print('statusCode: ${kek.statusCode}');
+}
+
+class NotificationCard extends StatelessWidget {
+  final String title;
+  final String customer;
+  final String price;
+  final String cashback;
+  const NotificationCard({
+    Key? key,
+    required this.title,
+    required this.customer,
+    required this.price,
+    required this.cashback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Text(
+              'Товар: $title',
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+          Container(
+            child: Text(
+              'id покупателя: $customer',
+              style: const TextStyle(color: Colors.grey, fontSize: 12.0),
+            ),
+          ),
+          Container(
+            child: Text(
+              'Цуна: $price',
+              style: const TextStyle(color: Colors.grey, fontSize: 12.0),
+            ),
+          ),
+          Container(
+            child: Text(
+              'cashback: $cashback',
+              style: const TextStyle(color: Colors.blueAccent),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
